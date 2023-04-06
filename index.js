8c9173c63907d650fa4422ed087bc3f071519442
Santa-Razvan <126788093+Santa-Razvan@users.noreplay.github.com>
2023-04-06 16:14:43 +0300

    made an event for each button that is being pressed, and made it do the calculation when the '=' button is clicked

--- index.js
+++ index.js
@@ -14,19 +14,50 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-let num1 = 1;
-let num2 = 2;
-let operator = '*';
+let num1 = '';
+let num2 = '';
+let operator = '';
 
 function operate(num1, num2, operator) {
     switch (operator) {
-        case '+': add(num1, num2);
+        case '+': return add(num1, num2);
             break;
-        case '-': subtract(num1, num2);
+        case '-': return subtract(num1, num2);
             break;
-        case '*': multiply(num1, num2);
+        case '*': return multiply(num1, num2);
             break;
-        case '/': divide(num1, num2);
+        case '/': return divide(num1, num2);
+            break;
         default: alert("This operator doesn't exist");
+            break;
     }
 }
+
+const buttons = document.querySelectorAll('button');
+const display = document.getElementById('display');
+let displayArray = [];
+let contor = 0;
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', (e) => {
+        display.textContent += e.target.value;
+        displayArray += e.target.value;
+        if (e.target.value === "=") {
+            for (let i = 0; i < displayArray.length - 1; i++) {
+                console.log(displayArray[i]);
+                if ((displayArray[i] === '+') ||
+                    (displayArray[i] === '-') ||
+                    (displayArray[i] === '*') ||
+                    (displayArray[i] === '/')) {
+                    operator = displayArray[i];
+                    contor++;
+                } else if (contor == 1) {
+                    num2 += displayArray[i];
+                } else num1 += displayArray[i];
+            }
+            display.textContent = "";
+            display.textContent = operate(+num1, +num2, operator);
+        }
+    });
+}
+
